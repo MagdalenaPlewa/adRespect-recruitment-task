b87b01235762c88f7119591e5105f567335a5256
MagdalenaPlewa <magdalenaplewa82@gmail.com>
2023-08-22 09:45:05 +0200

    code cleaning

--- src/main.js
+++ src/main.js
@@ -31,7 +31,6 @@ const sliderObj = {
         "Przykładowy tekst.",
         "test"
     ]
-
 }
 
 function search(){
@@ -79,7 +78,7 @@ function onClick(){
     })
 }
 
-function slider(i, popupImg){
+function slider(i){
     introImg.src = sliderObj.sliderImgs[i]
     introHeader.innerText = sliderObj.sliderHeaders[i]
     introText.innerText = sliderObj.sliderTexts[i]
